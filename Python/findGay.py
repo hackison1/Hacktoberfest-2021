03661d0c0b2c7e47d8060903d0deccf7f619fae1
hackison2 <91653690+hackison2@users.noreply.github.com>
2021-10-01 21:17:02 +0545

    Create  findGay.py
    program to find gay

---
+++ Python/findGay.py
@@ -0,0 +1,10 @@
+name = input("Enter the name: ")
+
+def findGay():
+  if(name.lower() == "apple" or name.lower() == "apeel"):
+    print("Gay found.")
+  else:
+    print(f"Sorry {name} is not a gay.")
+ 
+
+findGay()
