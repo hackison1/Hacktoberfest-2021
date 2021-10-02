12ed544e4240a23aad21338b7d303bc366c17234
AdityaWadkar <67093170+AdityaWadkar@users.noreply.github.com>
2021-10-02 10:36:16 +0530

    simple way to add two number
    I have just added a function which return addition of two numbers and also printed the result on output screen

--- Python/addTwoNumbers.py
+++ Python/addTwoNumbers.py
@@ -1,6 +1,6 @@
-def add(n1, n2):
-  try:
-      return int(n1) + int(n2)
-  except:
-      print("Please enter Numbers") 
+def add(n1,n2): #function to add two numbers
+    return n1+n2 # returns addition of two numbers
+n1=int(input('Enter first number : '))#takes first number from user
+n2=int(input('Enter second number : '))#takes second number from user
+print("Addition is : ",add(n1,n2))#print result on screen
   
