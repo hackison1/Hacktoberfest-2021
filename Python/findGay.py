d921a3ef665110ef7ee32d25b02e93582bd4b3bf
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2021-10-06 15:13:28 +0545

    Update findGay.py
    value updated

--- Python/findGay.py
+++ Python/findGay.py
@@ -1,7 +1,9 @@
+
+# sankalpa dahal is gay btw
 name = input("Enter the name: ")
 
 def findGay():
-  if(name.lower() == "apple" or name.lower() == "apeel"):
+  if(name.lower() == "apple" or name.lower() == "apeel" or name == "sankk"):
     print("Gay found.")
   else:
     print(f"Sorry {name} is not a gay.")
