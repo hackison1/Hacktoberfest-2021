b7c552e03b6339bd77a30008451ec49f06379bf8
TheCrysp <76145703+TheCrysp@users.noreply.github.com>
2021-10-09 22:40:12 +0545

    Create String_Gen.dart

---
+++ Dart/String_Gen.dart
@@ -0,0 +1,15 @@
+
+import 'dart:math';
+
+ 
+void main() {
+  print(getRandomString(5));    
+  print(getRandomString(10));    
+  print(getRandomString(15));    
+}
+
+const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+Random _rnd = Random();
+
+String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
+    length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
