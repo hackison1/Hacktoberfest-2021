17bbd1eee03b3ce64d4e9421335ce063d76577f7
abinash2525 <88371116+abinash2525@users.noreply.github.com>
2021-10-10 12:09:54 +0545

    Create mergeSort.js
    merge sort in js

---
+++ JavaScript/mergeSort.js
@@ -0,0 +1,16 @@
+function merge(left, right) {
+    let arr = []
+    // Break out of loop if any one of the array gets empty
+    while (left.length && right.length) {
+        // Pick the smaller among the smallest element of left and right sub arrays 
+        if (left[0] < right[0]) {
+            arr.push(left.shift())  
+        } else {
+            arr.push(right.shift()) 
+        }
+    }
+    
+    // Concatenating the leftover elements
+    // (in case we didn't go through the entire left or right array)
+    return [ ...arr, ...left, ...right ]
+}
