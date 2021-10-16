cf24c6a4bd69323a2a57a6b894bc7a308e460c73
gitagtm <92620225+gitagtm@users.noreply.github.com>
2021-10-16 11:58:31 +0545

    reverse_triangle_pattern.py

---
+++ reverse_triangle_pattern.py
@@ -0,0 +1,12 @@
+rows = int(input("Enter the number of rows: "))  
+k = 2 * rows - 2  
+
+for i in range(rows, -1, -1):  
+ 
+    for j in range(k, 0, -1):  
+        print(end=" ")  
+    k = k + 1  
+
+    for j in range(0, i + 1):  
+        print("*", end=" ")  
+    print("")  
