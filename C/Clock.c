45646b53b5cd68532f40b3f067d4b734272bb2d3
punchermaker <92624401+punchermaker@users.noreply.github.com>
2021-10-16 13:54:30 +0545

    update clock.c

--- C/Clock.c
+++ C/Clock.c
@@ -1,30 +1,3 @@
-/*#include <stdio.h>
-#include <time.h>
-int main(){
-    int hour, minute, second;
-    hour=minute=second=0;
-    while(1)
-    {    
-        printf("%02d : %02d : %02d \n",hour,minute,second);
-        second++;
-        if(second==59){
-            minute+=1;
-            second=0;
-        }
-        if(minute==60){
-            hour+=1;
-            minute=0;
-        }
-        if(hour==24){
-            hour=0;
-            minute=0;
-            second=0;
-        }    
-        sleep(1);
-    }
-    return 0;
-}
-*/
 #include <stdio.h>
 #include <time.h>
 int main(){
