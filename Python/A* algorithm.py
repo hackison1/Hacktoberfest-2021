3bdd02ad62735d9a60485bf418aca2df55366211
Pratap parui <52885495+pratap360@users.noreply.github.com>
2021-10-19 19:42:13 +0530

    A program that implement A* algorithm in AI

---
+++ Python/A* algorithm.py
@@ -0,0 +1,24 @@
+from simpleai.search import SearchProblem, astar
+
+GOAL = 'HACKTOBER FEST'
+class HelloProblem(SearchProblem):
+    def actions(self, state):
+        if len(state) < len(GOAL):
+            return list(' ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        else:
+            return []
+            
+    def result(self, state, action):
+        return state + action
+    def is_goal(self, state):
+        return state == GOAL
+    def heuristic(self, state):
+        # how far are we from the goal?
+        wrong = sum([1 if state[i] != GOAL[i] else 0 for i in range(len(state))]) 
+        missing = len(GOAL) - len(state) 
+        return wrong + missing
+
+problem = HelloProblem(initial_state='')
+result = astar(problem)
+print(result.state)
+print(result.path())
