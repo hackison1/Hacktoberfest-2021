2f7ebf3dbc5491d910a10267d4cd8c5b35639307
Shyam Radadiya <67785558+shyam-ra18@users.noreply.github.com>
2021-10-20 15:02:15 +0530

    update program
    old programm was wrong!!!

--- CPP/DecimalToBinary.cpp
+++ CPP/DecimalToBinary.cpp
@@ -1,14 +1,20 @@
-#include<iostream>
-using namespace std;
-//written by pankaj
-int main(){
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+int a[10], n, i;    
+cout<<"Enter the number to convert: ";    
+cin>>n; 
 
-    int n;
-    cin >> n;
-    while (n > 0){
-        cout << n%2;
-        n = n/2;
-    }
+    for(i=0; n>0; i++){    
+            a[i]=n%2;    
+            n= n/2;  
+    }    
+cout<<"Binary of the given number= ";    
 
-    return 0;
-}
+    for(i=i-1 ;i>=0 ;i--){    
+            cout<<a[i];    
+    }    
+} 
+
+// by:Shyam Radadiya
