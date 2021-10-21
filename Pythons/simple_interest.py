e93ebeb9a1189ee82b14ae6df4e0743b14dd0215
Johnstha321 <91748575+Johnstha321@users.noreply.github.com>
2021-10-21 22:24:07 +0545

    Update simple_interest.py

--- Pythons/simple_interest.py
+++ Pythons/simple_interest.py
@@ -1,12 +1,12 @@
 
 def simple_interest(p,t,r):
-	print('The principal is', p)
-	print('The time period is', t)
-	print('The rate of interest is',r)
+	print('The principal is equal to', p)
+	print('The time period is equal to', t)
+	print('The rate of interest is equal to',r)
 	
 	si = (p * t * r)/100
 	
-	print('The Simple Interest is', si)
+	print('The Simple Interest is equal to', si)
 	return si
 	
 simple_interest(8, 6, 8)
