add2e7f61a246c67a96729d7aee16043014f32ed
anushkaparadkar <anupara15@gmail.com>
2021-10-25 15:03:31 -0500

    added R code

---
+++ R/importing-dataset.R
@@ -0,0 +1,8 @@
+
+#1.Select file manually
+df = read.csv(file.choose())
+
+#2. Set working diirectory & read file
+getwd() #checking working directory
+setwd("directory route")  #setting working directory
+read.csv("filename")
