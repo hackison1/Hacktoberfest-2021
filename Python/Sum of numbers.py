11ca54edc5e0df0f50deef0ac0af9f609f60751e
Kunal Sah <65846265+Kunalsah29@users.noreply.github.com>
2021-10-26 18:34:25 +0545

    UPDATE sum of number

--- Python/Sum of numbers.py
+++ Python/Sum of numbers.py
@@ -1,4 +1,3 @@
 a = int(input("Enter a number:"))
-b = int(input("Enter another number:"))
-add = a+b
-print (f"The sum of these numbers is {add}.")
+b = int(input("Enter another number:")) 
+print (f"The sum of these numbers is {a+b}.")
